4b73c61ee1962e65f20f7f93b4f562ffd46fed85
liyonggang876 <15874461@qq.com>
2018-04-19 10:42:18 +0800

    第二次提交

--- src/com/duolabao/test/git/AutoGit.java
+++ src/com/duolabao/test/git/AutoGit.java
@@ -3,5 +3,6 @@ package com.duolabao.test.git;
 public class AutoGit {
     public static void main(String args[]){
         System.out.println("This is git");
+        System.out.println("ttt");
     }
 }
